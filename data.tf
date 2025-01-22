818d3c2780edb817deed183563d7e6637118b5fc
abhilash-kundoor-2393 <abhilash2393@gmail.com>
2025-01-22 15:49:12 -0500

    Include a data block to retrieve the AMI for the EC2 instance.

--- data.tf
+++ data.tf
@@ -1,3 +1,13 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
