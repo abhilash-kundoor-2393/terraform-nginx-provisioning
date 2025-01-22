1edb66e864b0bb8b3e6217ada205f2ba37929861
abhilash-kundoor-2393 <abhilash2393@gmail.com>
2025-01-22 15:41:31 -0500

    Add a route table and create a route to direct internet traffic to internet gateway

--- main.tf
+++ main.tf
@@ -38,3 +38,16 @@ resource "aws_internet_gateway_attachment" "internet_gateway_attachment" {
   internet_gateway_id = aws_internet_gateway.internet_gateway.id
   vpc_id              = aws_vpc.main.id
 }
+
+resource "aws_route_table" "internet_access_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "internet-access-route-table"
+  }
+}
